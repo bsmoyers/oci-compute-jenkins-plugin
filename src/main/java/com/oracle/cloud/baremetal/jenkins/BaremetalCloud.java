c5e279635f6fe7a98c7d5677ba49527da06608b9
Brian Moyers <3004489+bsmoyers@users.noreply.github.com>
2020-06-26 15:49:15 -0500

    clean up some logging and unneeded things

--- src/main/java/com/oracle/cloud/baremetal/jenkins/BaremetalCloud.java
+++ src/main/java/com/oracle/cloud/baremetal/jenkins/BaremetalCloud.java
@@ -19,7 +19,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
-import java.util.stream.Collectors;
 import java.net.InetAddress;
 
 import javax.servlet.ServletException;
@@ -310,27 +309,21 @@ public class BaremetalCloud extends AbstractCloudImpl{
     }
 
     public BaremetalCloudAgentTemplate getTemplate(Label label) {
-        LOGGER.log(Level.INFO, "iteratorTemplateId =  {0}",iteratorTemplateId );
         for (ListIterator<? extends BaremetalCloudAgentTemplate> iter = templates.listIterator(iteratorTemplateId); iter.hasNext(); ) {       
             BaremetalCloudAgentTemplate t = iter.next();
-            LOGGER.log(Level.INFO, "trying templateId {0}", t.templateId ); 
             if (t.getDisableCause() != null) {
                 continue;
             }
             if (t.getMode() == Node.Mode.NORMAL) {
                 if (label == null || label.matches(t.getLabelAtoms())) {
-                    LOGGER.log(Level.INFO, "matched on templateId {0} {1} ", new Object[]{ t.templateId, label } );
                     // increment the iterator start id on match
                     iteratorTemplateId = (iteratorTemplateId +1 ) % templates.size();
-                    LOGGER.log(Level.INFO, "new iteratorTemplateId =  {0}",iteratorTemplateId );
                     return t;
                 }
             } else if (t.getMode() == Node.Mode.EXCLUSIVE) {
                 if (label != null && label.matches(t.getLabelAtoms())) {
-                    LOGGER.log(Level.INFO, "matched on templateId {0} {1} ", new Object[] { t.templateId, label } );
                     // increment the iterator start id on match
                     iteratorTemplateId = (iteratorTemplateId +1 ) % templates.size();
-                    LOGGER.log(Level.INFO, "new iteratorTemplateId =  {0}",iteratorTemplateId );
                     return t;
                 }
             }
